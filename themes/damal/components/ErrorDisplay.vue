1931204ffc97a6c62f59184c6c73f70da5dd0bca
dmaizel <dmaizel@redhat.com>
2020-09-24 21:16:19 +0300

    Added authentication forms

---
+++ themes/damal/components/ErrorDisplay.vue
@@ -0,0 +1,23 @@
+<template>
+<ul>
+    <li v-for="error in errors" :key="error">{{ error }}</li>
+</ul>
+</template>
+
+<script>
+export default {
+    name: "ErrorDisplay",
+    props: {
+        errors: Array,
+    },
+};
+</script>
+
+<style scoped>
+li {
+    margin: 0;
+    padding: 0;
+    display: block;
+    color: red;
+}
+</style>
