698da4c7daaa33073b5e1c37bc780ac2cc3a067a
dmaizel <dmaizel@redhat.com>
2020-09-25 11:48:57 +0300

    Changed from passing props to using events

--- themes/damal/components/SigninForm.vue
+++ themes/damal/components/SigninForm.vue
@@ -1,6 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <Form
     button_title="Sign in"
     next_page="/signup"
     v-bind:onSubmit="onSubmit"
@@ -21,14 +19,7 @@
       type="password"
     />
   </Form>
-=======
-<Form buttonTitle="Sign in" nextPage="/signup" v-bind:onSubmit="onSubmit" message="Don't have an account yet? Sign Up" title="Sign in">
     <FormInput v-model="inputs.email" label="Email" name="email" type="text" v-bind:validators="validators.email" />
-    <FormInput v-model="inputs.password" label="Password" name="password" type="password" />
-</Form>
->>>>>>> 338ae1a... Changed to appropriate cases
-</template>
-
 <script>
 import FormInput from "./FormInput";
 import Form from "./Form";
