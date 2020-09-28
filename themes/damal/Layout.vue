5e260ff1d12328f6fd62f3c01d27eae314f12ee9
dmaizel <dmaizel@redhat.com>
2020-09-28 14:53:36 +0300

    Added sign in and sign up screen for classic theme

--- themes/damal/Layout.vue
+++ themes/damal/Layout.vue
@@ -1,10 +1,10 @@
 <template>
-<div class="container">
+  <div class="app">
     <Header />
     <nuxt />
     <Footer />
     <div id="fb-root"></div>
-</div>
+  </div>
 </template>
 
 <script>
@@ -12,16 +12,19 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 export default {
-    components: {
-        Header,
-        Footer,
-    },
-    head: {
-        link: [{
-            rel: "stylesheet",
-            href: "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400&display=swap",
-        }, ],
-    },
+  components: {
+    Header,
+    Footer,
+  },
+  head: {
+    link: [
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400&display=swap",
+      },
+    ],
+  },
 };
 </script>
 
@@ -29,47 +32,47 @@ export default {
 @import "colors";
 
 html {
-    font-family: "Open Sans", sans-serif;
-    font-size: 16px;
+  font-family: "Open Sans", sans-serif;
+  font-size: 16px;
 }
 
 html[dir="rtl"] {
-    text-align: right;
+  text-align: right;
 }
 
 *,
 *:before,
 *:after {
-    box-sizing: border-box;
-    margin: 0;
+  box-sizing: border-box;
+  margin: 0;
 }
 
 a {
-    text-decoration: none;
-    color: $mainColor;
+  text-decoration: none;
+  color: $mainColor;
 }
 
 a:hover {
-    text-decoration: underline;
+  text-decoration: underline;
 }
 
-.container {
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
+.app {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
 }
 
 @media all and (max-width: 500px) {
-    .container {
-        margin: 0.5rem;
-    }
+  .app {
+    margin: 0.5rem;
+  }
 }
 
 .row {
-    display: flex;
+  display: flex;
 
-    * {
-        flex: 1;
-    }
+  * {
+    flex: 1;
+  }
 }
 </style>
